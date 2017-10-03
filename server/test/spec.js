2a9527a5136a01037ba805cd9d5e42373fe708f0
Dinuka De Silva <l.dinukadesilva@gmail.com>
2017-10-03 18:32:19 +0530

    gh-6: Adding a test setup with `mocha` and `nyc` for code coverage

---
+++ server/test/spec.js
@@ -0,0 +1,23 @@
+var server = require("../src/app");;
+
+describe("MEVN-boilerplate - TESTS", function () {
+  after(function () {
+    process.exit();
+  });
+  describe("posts", function () {
+    describe("GET", function () {
+      it("should return all posts", function () {
+        //TODO
+      });
+    });
+    describe("POST", function () {
+      //TODO
+    });
+    describe("DELETE", function () {
+      //TODO
+    });
+    describe("PUT", function () {
+      //TODO
+    });
+  });
+});
